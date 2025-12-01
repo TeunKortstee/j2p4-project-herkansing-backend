83db9092e8f490fbcd643a67949e400ff43e60f8
TeunKortstee <teunkortstee@gmail.com>
2025-12-01 16:01:25 +0100

    starting commit for the api added spring initzializer

---
+++ Project/src/test/java/com/example24/Backend/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example24.Backend.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
